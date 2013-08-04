cdfcf2222c14e7c19a67ee494c29c03d2155a7db
Dan Michael O. Heggø <danmichaelo@gmail.com>
2013-08-04 11:54:17 +0200

    Image archive uploader: Show error messages

--- includes/classes/jqueryupload.php
+++ includes/classes/jqueryupload.php
@@ -13,6 +13,8 @@ class JqueryUpload {
         $template = file_get_contents($this->template_dir . $this->uploadform_template);
         $r1a[] = "%actionurl%";     $r2a[] = $this->actionurl;
         $r1a[] = "%completeurl%";   $r2a[] = $this->completeurl;
+        $r1a[] = "%upload_max_filesize%";   $r2a[] = ini_get('upload_max_filesize');
+
         // $r1a[] = "%errorurl%";      $r2a[] = $this->errorurl;
         // $r1a[] = "%maximagesize%";  $r2a[] = $this->maximagesize;
         // $r1a[] = "%jupload_dir%";   $r2a[] = $this->jupload_dir;
