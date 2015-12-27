f08600b08bcd63d0abd2d91f114a4f70a2ed5719
Dan Michael O. Heggø <danmichaelo@gmail.com>
2015-12-27 17:56:48 +0100

    Fix ckfinder db connection

--- www/libs/ckfinder-2.6.0/core/connector/php/bergenvs_plugin.php
+++ www/libs/ckfinder-2.6.0/core/connector/php/bergenvs_plugin.php
@@ -1,5 +1,9 @@
 <?php
 
+require_once(dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__))))))) . '/vendor/autoload.php');
+$dotenv = new Dotenv\Dotenv(dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__))))))));
+$dotenv->load();
+
 define('BG_INC_PATH',dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__))))))).'/includes/');
 require_once(BG_INC_PATH.'head.php');
 require_once(BG_CLASS_PATH.'ckfinderplugin.php');
