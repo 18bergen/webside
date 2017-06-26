93fd5610c02aeae53b47275dd9b04d785ba9e508
Webmaster 18bergen.org <webmaster@18bergen.org>
2017-06-26 15:33:26 +0200

    IE6: Gufs fra fortiden

--- includes/templates/site_main_normal.php
+++ includes/templates/site_main_normal.php
@@ -1,27 +1,6 @@
 		<div id="main_col" class="column">
     		<div class="inner_col">
    				
-   				<!--[if lte IE 6]>
-					<style type="text/css">
-						#ie6msg{border:3px solid #090; margin:8px 0; background:#cfc; color:#000;}
-						#ie6msg h3{margin:8px; padding:0;}
-						#ie6msg p{margin:8px; padding:0;}
-						#ie6msg p a.getie7{font-weight:bold; color:#006;}
-						#ie6msg p a.ie6expl{font-weight:normal; color:#006;}
-					</style>
-					<div id="ie6msg">
-						<h3>Tips fra oss: Du har en eldre versjon av nettleseren Internet Explorer.</h3>
-						<p>
-							For å få en best mulig opplevelse av å bruke våre nettsider,<br />
-							kan du gratis <a class="getie7" href="http://www.microsoft.com/norge/windows/downloads/ie/getitnow.mspx" target="_blank">hente en nyere versjon av Internet Explorer</a>.<br />
-							Bruker du en jobb-PC bør du kontakte IT-ansvarlig.
-						</p>
-						<p>
-							<a class="ie6expl" href="http://www.tu.no/it/article199563.ece" target="_blank">Les mer om hvorfor vi anbefaler denne oppgraderingen hos Teknisk Ukeblad.</a>
-						</p>
-					</div>
-				<![endif]-->  
-   				
    				%content%
    
  	  		</div>
