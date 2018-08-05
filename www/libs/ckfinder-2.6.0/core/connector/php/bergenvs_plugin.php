c592b5fe41e63e31461dec3d02a24ed69796a3d4
Dan Michael O. Heggø <danmichaelo@gmail.com>
2018-08-05 07:56:21 +0200

    Add sentry to ckfinder

--- www/libs/ckfinder-2.6.0/core/connector/php/bergenvs_plugin.php
+++ www/libs/ckfinder-2.6.0/core/connector/php/bergenvs_plugin.php
@@ -1,12 +1,18 @@
 <?php
 
-require_once(dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__))))))) . '/vendor/autoload.php');
-$dotenv = new Dotenv\Dotenv(dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__))))))));
+define('BG_ROOT_PATH', dirname(dirname(dirname(dirname(dirname(dirname(__DIR__)))))));
+define('BG_INC_PATH', BG_ROOT_PATH . '/includes/');
+
+require_once(BG_ROOT_PATH . '/vendor/autoload.php');
+
+$dotenv = new Dotenv\Dotenv(BG_ROOT_PATH);
 $dotenv->load();
 
-define('BG_INC_PATH',dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__))))))).'/includes/');
-require_once(BG_INC_PATH.'head.php');
-require_once(BG_CLASS_PATH.'ckfinderplugin.php');
+$ravenClient = new Raven_Client(getenv('SENTRY_DSN'));
+$ravenClient->install();
+
+require_once(BG_INC_PATH . 'head.php');
+require_once(BG_CLASS_PATH . 'ckfinderplugin.php');
 
 $bg18 = new ckFinderPlugin();
 $dp0->prepareClassInstance($bg18);
