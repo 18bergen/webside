e6410ff79df5e93a0039700b1f9ff219118c8225
Webmaster 18bergen.org <webmaster@18bergen.org>
2020-09-20 15:58:51 +0200

    Add config file to version control

---
+++ includes/config.php
@@ -0,0 +1,26 @@
+<?php
+
+if ( !defined('DBPREFIX') ) {
+    define('DBPREFIX', 'bg_'); // no leading slash, no trailing slash
+}
+
+$queryCount = 0;
+
+$months = array("Januar","Februar","Mars","April","Mai","Juni","Juli","August","September","Oktober","November","Desember");
+$smonths = array("Jan","Feb","Mars","Apr","Mai","Juni","Juli","Aug","Sept","Okt","Nov","Des");
+
+$passwordLengthLimit = 40;
+
+$memberlist_class = 9;
+$memberlist_page = 85;
+
+$sidebar_wordbox = true;
+$wordbox_class = 21;
+$wordbox_page = 131;
+$changelog_page = 163;
+$sidebar_poll = true;
+$poll_class = 30;
+$poll_page = 203;
+
+
+?>
